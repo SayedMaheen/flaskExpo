af525b00c990759ca057d0e72ae8c44a54381f40
Sayed Maheen <mhnpta@gmail.com>
2021-06-22 15:51:17 +0530

    Commit before javascript function for filter

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import csv
+
+app = Flask(__name__)
+@app.route("/home")
+def index():
+  with open('Untitled.csv') as csv_file:
+    data = csv.reader(csv_file, delimiter=',')
+    first_line = True
+    places = []
+    for row in data:
+      if not first_line:
+        places.append({
+            
+          "States": row[0],
+          "Capital": row[1],
+          "Language":row[2]
+
+        })
+      else:
+        first_line = False
+    
+  return render_template("home.html", xlog=places)
+@app.route('/')
+def projectexpo():
+    return render_template('home.html', name='Nick')
